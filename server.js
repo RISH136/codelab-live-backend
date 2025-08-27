b5e43c2576762e9108a769e16962b679d053a09d
Rishabh393 <rishabhdev1392467@gmail.com>
2025-08-27 16:22:43 +0530

    changed frontend url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT=process.env.PORT || 3000;
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin: 'http://localhost:5173',
+        origin: 'codelab-live-frontend.vercel.app',
         credentials: true,
     }
 });
